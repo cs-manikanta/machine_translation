d70f711cc7865614aeb5a23f4eb45a712d6141c6
manikanta_cherukuri <99506148+cs-manikanta@users.noreply.github.com>
2022-11-21 18:17:50 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ trans = Translator()
 
 input_text = st.text_area('Enter your text', height = 100)
 
-Target_Language =st.selectbox('Select the target language' , ('English''Telugu','Hindi','Bengali','Gujarati','Kannada','Malayalam','Marathi','Odia','Punjabi','Tamil','Urdu','Arabic','German'))
+Target_Language =st.selectbox('Select the target language' , ('English','Telugu','Hindi','Bengali','Gujarati','Kannada','Malayalam','Marathi','Odia','Punjabi','Tamil','Urdu','Arabic','German'))
 st.write('You selected: ', Target_Language)
 
 if st.button('Translate'):
